68e92ac0beaa81da92caf44ac36c0b16371743ad
bightg30098 <bightg30098@gmail.com>
2021-08-08 18:42:47 +0800

    fix weather id

--- src/services/weather.js
+++ src/services/weather.js
@@ -7,7 +7,7 @@ export const weatherApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org/data/2.5/' }),
   endpoints: (builder) => ({
     getWeatherById: builder.query({
-      query: (id = '6167865') =>
+      query: (id = '1668338') =>
         `weather?id=${id}&appid=12e58ba7c668780614ff867c4d0d91c3&units=metric`,
     }),
   }),
