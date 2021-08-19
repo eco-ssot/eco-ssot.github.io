a06a80831e79b57fcbaf5bc8e9fbe38ef68b21d2
bightg30098 <bightg30098@github.com>
2021-08-19 16:42:48 +0800

    fix ratio formatter

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -6,8 +6,10 @@ export const originalFormatter = (value) => get(value, 'value', value);
 export const baseFormatter = (value, option = {}) =>
   toFormattedNumber(get(value, 'value', value), option);
 
-export const ratioFormatter = (value, option = {}) =>
-  toFormattedNumber(get(value, 'value', value), { unit: 1e-2, suffix: '%', ...option });
+export const ratioFormatter = (value, { precision = 0 } = {}) => {
+  const val = (get(value, 'value', value) / 1e-2).toFixed(precision);
+  return isNaN(val) ? '-' : `${val}%`;
+};
 
 export const keepPrecisionFormatter = (value, option = {}) =>
   toFormattedNumber(get(value, 'value', value), { ...option, keepPrecision: true });
