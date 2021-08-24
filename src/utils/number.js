1101c8c3bb9cbe8d57f0d20ed7d85c2297457fb4
bightg30098 <bightg30098@github.com>
2021-08-24 16:56:38 +0800

    refactor formatted number

--- src/utils/number.js
+++ src/utils/number.js
@@ -1,30 +1,31 @@
-import currency from 'currency.js';
-
 export function toFormattedNumber(
   value,
   {
-    separator = ',',
-    symbol = '',
-    precision = 0,
     unit = 1,
+    precision = 0,
     suffix = '',
     defaultValue = '-',
     keepPrecision = false,
     maxPrecision = 3,
   } = {}
 ) {
-  try {
-    const val = currency(value, { errorOnInvalid: true });
-    return `${currency(val.divide(unit), {
-      separator,
-      symbol,
-      errorOnInvalid: true,
-      precision,
-      ...(keepPrecision && {
-        precision: Math.min((String(value).split('.')[1] || '').length, maxPrecision),
-      }),
-    }).format()}${suffix}`;
-  } catch {
+  if (/Infinity/gi.test(String(value))) {
+    return `${value}${suffix}`;
+  }
+
+  let nextPrecision = precision;
+  if (keepPrecision) {
+    nextPrecision = Math.min((String(value).split('.')[1] || '').length, maxPrecision);
+  }
+
+  const formattedValue = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: nextPrecision,
+    maximumFractionDigits: nextPrecision,
+  }).format(value / unit);
+
+  if (/NaN|∞/gi.test(String(formattedValue))) {
     return defaultValue;
   }
+
+  return `${formattedValue}${suffix}`;
 }
