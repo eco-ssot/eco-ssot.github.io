8d8d6558f00a473452405a781a36423682693f5f
bightg30098 <bightg30098@github.com>
2021-08-24 17:07:24 +0800

    add version info

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,15 +12,17 @@ import Avatar from '../avatar/Avatar';
 import { navigate } from '../../router/helpers';
 import { selectBusiness } from '../../renderless/query-params/queryParamsSlice';
 import APP_CONFIG from '../../constants/app-config';
+import packageJson from '../../../package.json';
 
 export default function Header({ className }) {
   const business = useSelector(selectBusiness);
   const { keycloak } = useKeycloak();
   return (
     <div className={clsx('flex px-4 bg-primary-800 shadow-lg items-center z-10', className)}>
-      <Link className="flex items-center font-medium text-2xl space-x-4" to="/">
+      <Link className="flex items-center font-medium text-2xl space-x-4 truncate" to="/">
         <img className="h-10 w-10" src="/logo-64x64.png" alt="logo" />
         <div className="block truncate">ESG 績效管理平台</div>
+        <div className="block truncate text-unit text-sm">Ver {packageJson.version}</div>
       </Link>
       <Divider className="h-1/2" />
       {keycloak?.authenticated ? (
