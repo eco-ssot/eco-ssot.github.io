96afdbb0269b4f08ae74737c5b9dcea868941ad7
10503052 <leo_jhuo@wistron.com>
2021-09-15 15:35:54 +0800

    fix keycloak json error

--- src/keycloak/keycloak.js
+++ src/keycloak/keycloak.js
@@ -1,3 +1,9 @@
 import Keycloak from 'keycloak-js';
 
-export default Keycloak();
+const { REACT_APP_KEYCLOAK_REALM, REACT_APP_KEYCLOAK_URL, REACT_APP_KEYCLOAK_CLIENT_ID } = process.env;
+
+export default Keycloak({
+  realm: REACT_APP_KEYCLOAK_REALM,
+  url: REACT_APP_KEYCLOAK_URL,
+  clientId: REACT_APP_KEYCLOAK_CLIENT_ID,
+});
