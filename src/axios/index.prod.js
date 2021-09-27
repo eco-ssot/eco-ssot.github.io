db0244bbd79459accf83bdd88f544e31dc8d1ba7
10503052 <leo_jhuo@wistron.com>
2021-09-27 10:48:02 +0800

    fix keycloak import

--- src/axios/index.prod.js
+++ src/axios/index.prod.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 
 import tokenInterceptor from './tokenInterceptor';
 
+const { REACT_APP_KEYCLOAK_REALM, REACT_APP_KEYCLOAK_URL } = process.env;
+
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
+const keycloakInstance = axios.create({
+  baseURL: `${REACT_APP_KEYCLOAK_URL}/admin/realms/${REACT_APP_KEYCLOAK_REALM}`,
+});
+
 instance.interceptors.request.use(tokenInterceptor);
+keycloakInstance.interceptors.request.use(tokenInterceptor);
 
 export default instance;
