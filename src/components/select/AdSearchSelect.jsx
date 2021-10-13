5ea583d8bd8a8264dac56a586db6f6d0b33cf139
10503052 <leo_jhuo@wistron.com>
2021-10-13 13:19:51 +0800

    update graph url

--- src/components/select/AdSearchSelect.jsx
+++ src/components/select/AdSearchSelect.jsx
@@ -2,11 +2,9 @@ import axios from 'axios';
 
 import { AsyncSearchSelect } from './SearchSelect';
 
-const { REACT_APP_GRAPH_API_BASE_URL, REACT_APP_GRAPH_API_CODE } = process.env;
-
 const loadOptions = (q, callback) => {
   return axios
-    .get(`${REACT_APP_GRAPH_API_BASE_URL}?code=${REACT_APP_GRAPH_API_CODE}${q ? `&q=${q}` : ''}`)
+    .get(`${process.env.REACT_APP_API_BASE_URL}/graph${q ? `?q=${q}` : ''}`)
     .then((res) => {
       const options = res.data.map(({ id, mail }) => ({
         value: id,
