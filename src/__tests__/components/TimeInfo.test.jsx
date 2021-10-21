29aa33322973b7b3178a62d86402db94411a4830
10503052 <leo_jhuo@wistron.com>
2021-10-21 16:11:09 +0800

    fix date

--- src/__tests__/components/TimeInfo.test.jsx
+++ src/__tests__/components/TimeInfo.test.jsx
@@ -6,16 +6,8 @@ import TimeInfo from '../../components/time-info/TimeInfo';
 test('TimeInfo', () => {
   MockDate.set('2021-10-21');
 
-  const { asFragment } = render(<TimeInfo />);
-  expect(asFragment()).toMatchInlineSnapshot(`
-    <DocumentFragment>
-      <div
-        class="block truncate"
-      >
-        2021年 10月 21日 08:00
-      </div>
-    </DocumentFragment>
-  `);
+  const { queryByText } = render(<TimeInfo />);
+  expect(queryByText(/2021年 10月 21日/i)).toBeInTheDocument();
 
   MockDate.reset();
 });
