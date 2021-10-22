35bd48e569d8c37e12534475c1d4e20a50d21742
10503052 <leo_jhuo@wistron.com>
2021-10-22 17:31:06 +0800

    hide upload button

--- src/pages/waste/WasteTable.jsx
+++ src/pages/waste/WasteTable.jsx
@@ -129,7 +129,7 @@ const HEADERS = ({ business, pct, maxDate, baseYear = APP_CONFIG.BASE_YEAR_WASTE
           <div className="relative ">
             {value}
             <IconButton
-              className="absolute ml-2 p-1 bg-primary-600 rounded-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary-900 focus:ring-primary-600"
+              className="hidden absolute ml-2 p-1 bg-primary-600 rounded-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary-900 focus:ring-primary-600"
               onClick={() => setOpen(true)}>
               <UploadIcon className="w-5 h-5" />
             </IconButton>
