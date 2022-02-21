07cb57b7145abbc3c494856aceac63121934ef16
10503052 <leo_jhuo@wistron.com>
2022-02-21 15:19:44 +0800

    set ga user id

--- src/renderless/location/Location.jsx
+++ src/renderless/location/Location.jsx
@@ -4,13 +4,22 @@ import ReactGA from 'react-ga';
 import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
+import { useKeycloak } from '../../keycloak';
+
 import { setQueryParams, setHash } from './locationSlice';
 
 export default function Location() {
   const { pathname, search, hash } = useLocation();
+  const { keycloak } = useKeycloak();
   const dispatch = useDispatch();
   useEffect(() => dispatch(setQueryParams(search)), [search, dispatch]);
   useEffect(() => dispatch(setHash(hash)), [hash, dispatch]);
-  useEffect(() => ReactGA.pageview(pathname + search), [pathname, search]);
+  useEffect(() => {
+    const { given_name = '', preferred_username = '' } = keycloak?.idTokenParsed || {};
+    if (given_name && preferred_username) {
+      ReactGA.set({ userId: `${preferred_username} ${given_name}` });
+      ReactGA.pageview(pathname + search);
+    }
+  }, [pathname, search, keycloak?.idTokenParsed]);
   return null;
 }
