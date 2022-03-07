d7d49c49fbe034990b1b764977f5868b3fd328fe
10503052 <leo_jhuo@wistron.com>
2022-03-07 17:42:45 +0800

    fix plant query

--- src/hooks/usePlantPermission.js
+++ src/hooks/usePlantPermission.js
@@ -1,11 +1,15 @@
 import { useMemo } from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { useKeycloak } from '../keycloak';
+import { selectBusiness } from '../renderless/location/locationSlice';
 import { useGetPlantsQuery } from '../services/app';
 
 export default function usePlantPermission() {
   const { keycloak } = useKeycloak();
-  const { data } = useGetPlantsQuery();
+  const bo = useSelector(selectBusiness);
+  const { data } = useGetPlantsQuery({ bo });
   const plantPermission = useMemo(() => {
     if (keycloak?.realmAccess?.roles?.includes('DEV') || keycloak?.realmAccess?.roles?.includes('WZS-8')) {
       return data;
