58fb9a8388dddc5b994eb0e1df8b89fb259eb105
bightg30098 <bightg30098@gmail.com>
2022-03-13 12:11:45 +0800

    update pciture

--- src/components/picture/Picture.jsx
+++ src/components/picture/Picture.jsx
@@ -1,7 +1,16 @@
-export default function Picture({ className, src, fallback, alt, ...rest }) {
+export default function Picture({
+  className,
+  src,
+  fallback,
+  alt,
+  srcType = 'image/webp',
+  fallbackType = 'image/png',
+  ...rest
+}) {
   return (
     <picture className={className}>
-      <source srcSet={src} />
+      <source srcSet={src} type={srcType} />
+      <source srcSet={fallback} type={fallbackType} />
       <img src={fallback} alt={alt} className={className} {...rest} />
     </picture>
   );
