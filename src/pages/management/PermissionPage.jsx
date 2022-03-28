10c9dca149cb93eb6c532a4aa204002841d35952
10503052 <leo_jhuo@wistron.com>
2022-03-28 10:18:21 +0800

    add dev list

--- src/pages/management/PermissionPage.jsx
+++ src/pages/management/PermissionPage.jsx
@@ -16,20 +16,26 @@ export default function PermissionPage() {
   return (
     <div className="row-span-2 col-span-7">
       <div className="flex flex-col bg-primary-900 rounded shadow p-4 h-full">
-        <div className="text-xl font-medium mb-6">權限管理</div>
+        <div className="text-xl font-medium mb-6">權限名單</div>
         <div className="flex space-x-8 mb-2">
-          <div className="w-1/2 text-xl font-medium">P8廠數據可查看權限名單</div>
-          <div className="w-1/2 text-xl font-medium">後台參數可設定權限名單</div>
+          <div className="w-1/3 text-xl font-medium">P8廠數據可查看權限名單</div>
+          <div className="w-1/3 text-xl font-medium">後台參數可設定權限名單</div>
+          <div className="w-1/3 text-xl font-medium">開發群組名單</div>
         </div>
         <div className="flex overflow-auto space-x-8 ">
-          <div className="flex w-1/2">
+          <div className="flex w-1/3">
             <div className="flex flex-col flex-grow rounded-t-lg mb-1 overflow-auto shadow">
-              <Table columns={columns} data={[...wzs8Users, ...devUsers]} />
+              <Table columns={columns} data={wzs8Users} />
             </div>
           </div>
-          <div className="flex w-1/2">
+          <div className="flex w-1/3">
             <div className="flex flex-col flex-grow rounded-t-lg mb-1 overflow-auto shadow">
               <Table columns={columns} data={target_maintainer} />
+            </div>
+          </div>
+          <div className="flex w-1/3">
+            <div className="flex flex-col flex-grow rounded-t-lg mb-1 overflow-auto shadow">
+              <Table columns={columns} data={devUsers} />
             </div>
           </div>
         </div>
