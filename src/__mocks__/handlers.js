a9ff5afb68e0a70ebff320797126fabe18be311c
10503052 <leo_jhuo@wistron.com>
2022-04-15 09:59:05 +0800

    add mock response api delay

--- src/__mocks__/handlers.js
+++ src/__mocks__/handlers.js
@@ -6,7 +6,7 @@ export const handlers = [
     const pathname = req.url.pathname.split('/api/')[1];
     req.url.searchParams.sort();
     const search = req.url.searchParams.toString();
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await new Promise((resolve) => setTimeout(resolve, process.env.REACT_APP_MOCK_API_RESPONSE_DELAY));
     return import(`./${method}/${pathname}/${search}`)
       .then((data) => res(ctx.json(data.default)))
       .catch(() => import(`./${method}/${pathname}`).then((data) => res(ctx.json(data.default))))
