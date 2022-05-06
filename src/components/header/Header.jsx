84700770326b350f60eb9f7d701d3e6bebb01706
10503052 <leo_jhuo@wistron.com>
2022-05-06 18:09:12 +0800

    fix truncated header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,10 @@
+import { useEffect, useRef, useState } from 'react';
+
 import clsx from 'clsx';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useWindowSize } from 'react-use';
 
 import APP_CONSTANTS from '../../app/appConstants';
 import useSitePlantOptions from '../../hooks/useSitePlantOptions';
@@ -18,6 +21,9 @@ import GhostSelect from '../select/GhostSelect';
 import GroupSelect from '../select/GroupSelect';
 
 export default function Header({ className }) {
+  const ref = useRef();
+  const checkTruncatedRef = useRef(true);
+  const { width } = useWindowSize();
   const { t } = useTranslation(['common']);
   const { keycloak } = useKeycloak();
   const lng = useSelector(selectLanguage);
@@ -26,9 +32,29 @@ export default function Header({ className }) {
   const plant = useSelector(selectP);
   const sitePlantOptions = useSitePlantOptions();
   const { data: version } = useGetVersionQuery();
+  const [isTruncated, setIsTruncated] = useState(false);
   const navigate = useNavigate();
+  useEffect(() => {
+    ref.current && setIsTruncated(ref.current.offsetWidth < ref.current.scrollWidth);
+    checkTruncatedRef.current = true;
+  }, [width]);
+
   return (
-    <div className={clsx('z-10 flex items-center bg-primary-800 px-4 shadow-lg', className)}>
+    <div
+      className={clsx('z-10 flex items-center bg-primary-800 px-4 shadow-lg', className)}
+      ref={(node) => {
+        ref.current = node;
+        if (node) {
+          const truncated = node.offsetWidth < node.scrollWidth;
+          if (checkTruncatedRef.current) {
+            setIsTruncated(truncated);
+          }
+
+          if (truncated) {
+            checkTruncatedRef.current = false;
+          }
+        }
+      }}>
       <Link className="flex items-center space-x-4" to="/">
         <Picture className="h-10 w-10" src="/logo-64x64.webp" fallback="/logo-64x64.png" alt="logo" />
         <Ellipsis label={t('title')} className="text-xl font-medium" />
@@ -64,7 +90,7 @@ export default function Header({ className }) {
             ariaLabel="site-plant"
           />
           <Divider className="h-1/2" />
-          <NavBar />
+          <NavBar isTruncated={isTruncated} />
           <Divider className="h-1/2" />
           <Manual lng={lng} />
           <Divider className="h-1/2" />
--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,3 +1,7 @@
+import { Fragment } from 'react';
+
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/outline';
 import clsx from 'clsx';
 import qs from 'query-string';
 import { useTranslation } from 'react-i18next';
@@ -12,38 +16,76 @@ export function preloadElements({ element, routes }) {
   routes?.forEach(preloadElements);
 }
 
-export default function NavBar({ className }) {
-  const { t } = useTranslation(['location']);
+export default function NavBar({ className, isTruncated }) {
   const { pathname, search } = useLocation();
+  const { t } = useTranslation(['location']);
   return (
     <div className={clsx('flex flex-grow space-x-4', className)}>
-      {privateRoutes
+      {(isTruncated ? privateRoutes.slice(0, 6) : privateRoutes)
         .filter((route) => !route.hidden)
-        .map(({ index, indexPath, path, i18nKey, element, routes }) => {
-          return (
-            <div
-              onMouseEnter={() => preloadElements({ element, routes })}
-              aria-label={`nav-${i18nKey}`}
-              key={i18nKey}
-              className={
-                isMatched(pathname)({ index, indexPath, path })
-                  ? 'border-primary-600 text-gray-50 inline-flex items-center px-1 pt-1 border-b-2'
-                  : 'border-b-2 border-primary-800 text-gray-200 hover:text-gray-50 inline-flex items-center px-1 pt-1'
-              }
-            >
-              <Link
-                to={{
-                  pathname: path,
-                  state: { from: pathname },
-                  search: qs.pick(search, APP_CONSTANTS.GLOBAL_QUERY_KEYS),
-                }}
-                className="text-current text-lg font-medium"
-              >
-                <span className="block truncate">{t(i18nKey)}</span>
-              </Link>
-            </div>
-          );
-        })}
+        .map(({ index, indexPath, path, i18nKey, element, routes }) => (
+          <div
+            onMouseEnter={() => preloadElements({ element, routes })}
+            aria-label={`nav-${i18nKey}`}
+            key={i18nKey}
+            className={clsx(
+              'inline-flex items-center border-b-2 px-1 pt-1',
+              isMatched(pathname)({ index, indexPath, path })
+                ? 'border-primary-600 text-gray-50'
+                : 'border-primary-800 text-gray-200 hover:text-gray-50'
+            )}>
+            <Link
+              to={{
+                pathname: path,
+                state: { from: pathname },
+                search: qs.pick(search, APP_CONSTANTS.GLOBAL_QUERY_KEYS),
+              }}
+              className="text-lg font-medium text-current">
+              <span className="block truncate">{t(i18nKey)}</span>
+            </Link>
+          </div>
+        ))}
+      {isTruncated && (
+        <div className="relative">
+          <Menu as="div" className="relative inline-block text-left">
+            <Menu.Button className="inline-flex w-full items-center justify-center rounded-md py-2 px-3 text-lg font-medium text-gray-50 hover:bg-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+              More
+              <ChevronDownIcon className="ml-2 -mr-1 h-5 w-5 text-gray-200 hover:text-gray-50" aria-hidden="true" />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95">
+              <Menu.Items className="absolute right-0 mt-1 w-48 origin-top-right rounded-md border border-divider bg-primary-900 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                {privateRoutes.slice(6).map(({ index, indexPath, path, i18nKey, element, routes }) => (
+                  <div className="px-1 py-1" key={i18nKey}>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          to={{
+                            pathname: path,
+                            state: { from: pathname },
+                            search: qs.pick(search, APP_CONSTANTS.GLOBAL_QUERY_KEYS),
+                          }}
+                          className={clsx(
+                            'group flex w-full items-center rounded-md px-2 py-2 text-lg font-medium text-current',
+                            active ? 'bg-primary-600 text-gray-50' : 'text-gray-200 hover:text-gray-50'
+                          )}>
+                          <span className="block truncate">{t(i18nKey)}</span>
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+      )}
     </div>
   );
 }
