2de68741bc595d6cd0677755d0f9aa58dc11c374
10503052 <leo_jhuo@wistron.com>
2022-06-24 11:57:18 +0800

    add plantchangelog i18n

--- src/pages/management/PlantChangelogPage.jsx
+++ src/pages/management/PlantChangelogPage.jsx
@@ -1,23 +1,32 @@
 import { useMemo } from 'react';
 
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+
 import Table from '../../components/table/Table';
+import { selectLanguage } from '../../renderless/location/locationSlice';
 import { useGetPlantChangelogQuery } from '../../services/public';
 
-const COLUMNS = [
-  { Header: '異動類型', accessor: 'type' },
+const COLUMNS = (t) => [
+  { Header: t('managementPage:plantChangelog.changeType'), accessor: 'type' },
   { Header: 'Plant', accessor: 'plant' },
-  { Header: '異動後', accessor: 'after' },
-  { Header: '說明', accessor: 'desc' },
-  { Header: '異動日期', accessor: 'date' },
+  { Header: t('managementPage:plantChangelog.after'), accessor: 'after' },
+  { Header: t('managementPage:plantChangelog.description'), accessor: 'desc' },
+  { Header: t('managementPage:plantChangelog.changeDate'), accessor: 'date' },
 ];
 
 export default function PlantChangelogPage() {
-  const { data } = useGetPlantChangelogQuery();
-  const columns = useMemo(() => COLUMNS, []);
+  const { t } = useTranslation(['managementPage']);
+  const lng = useSelector(selectLanguage);
+  const { data } = useGetPlantChangelogQuery(undefined, {
+    selectFromResult: ({ data }) => ({ data: data?.[String(lng).split('-')[0]] }),
+  });
+
+  const columns = useMemo(() => COLUMNS(t), [t]);
   return (
     <div className="col-span-7 row-span-2">
       <div className="flex h-full flex-col space-y-6 rounded bg-primary-900 p-4 shadow">
-        <div className="text-xl font-medium">廠區異動</div>
+        <div className="text-xl font-medium">{t('managementPage:plantChangelog.title')}</div>
         <div className="flex flex-col overflow-auto rounded-t-lg shadow">
           <Table columns={columns} data={data || []} />
         </div>
