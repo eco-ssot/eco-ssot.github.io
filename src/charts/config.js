dbfe432ddbf8ec82f41f36a116a9a8b386f03232
10503052 <leo_jhuo@wistron.com>
2022-08-23 14:28:24 +0800

    update default chart text font family

--- src/charts/config.js
+++ src/charts/config.js
@@ -24,4 +24,7 @@ export const darkTheme = {
   axisLabel: {
     color: colors.gray['50'],
   },
+  textStyle: {
+    fontFamily: `ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
+  },
 };
