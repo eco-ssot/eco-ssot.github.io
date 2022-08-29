9fa815b17fe5fcbf4eecb4ff4440c60b99e02b1c
10503052 <leo_jhuo@wistron.com>
2022-08-29 13:34:36 +0800

    fix get mock handler

--- src/__mocks__/handlers.js
+++ src/__mocks__/handlers.js
@@ -1,27 +1,31 @@
 import { rest } from 'msw';
 
 const globs = import.meta.glob('./**/*.json');
-const mocks = {};
 
-for (const path in globs) {
-  globs[path]().then((mod) => {
-    mocks[path.replace('index.json', '').replace('.json', '')] = mod.default;
-  });
+async function getMock({ method, pathname, search }) {
+  if (search) {
+    const path = `./${method}/${pathname}${search}.json`;
+    if (globs[path]) {
+      return (await globs[path]()).default;
+    }
+  }
+
+  const nextPathname = pathname.slice(-1) === '/' ? pathname : `${pathname}/`;
+  const path = `./${method}/${nextPathname}index.json`;
+  if (globs[path]) {
+    return (await globs[path]()).default;
+  }
 }
 
 async function baseHandler(req, res, ctx) {
   const method = req.method.toLowerCase();
   const pathname = req.url.pathname.split('/api/')[1];
-  const nextPathname = pathname.slice(-1) === '/' ? pathname : `${pathname}/`;
   req.url.searchParams.sort();
   const search = req.url.searchParams.toString();
   await new Promise((resolve) => setTimeout(resolve, import.meta.env.VITE_MOCK_API_RESPONSE_DELAY));
-  if (mocks[`./${method}/${nextPathname}${search}`]) {
-    return res(ctx.json(mocks[`./${method}/${nextPathname}${search}`]));
-  }
-
-  if (mocks[`./${method}/${nextPathname}`]) {
-    return res(ctx.json(mocks[`./${method}/${nextPathname}`]));
+  const data = await getMock({ method, pathname, search });
+  if (data) {
+    return res(ctx.json(data));
   }
 
   return res(ctx.status(404));
